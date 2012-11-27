1a102f9f682115c2ed00d89688ebcd1ed8ad6966
Chirayu Desai <cdesai@cyanogenmod.org>
2012-11-27 12:39:24 +0530

    Power menu: allow enabling user switcher
    Change-Id: I089efbf2bec2339cb2630e597aa3778b6100c9c0

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3088,6 +3088,12 @@ public final class Settings {
         public static final String POWER_MENU_AIRPLANE_ENABLED = "power_menu_airplane_enabled";
 
         /**
+         * Whether power menu user switcher is enabled
+         * @hide
+         */
+        public static final String POWER_MENU_USER_ENABLED = "power_menu_user_enabled";
+
+        /**
          * Whether power menu silent mode is enabled
          * @hide
          */
@@ -3176,6 +3182,7 @@ public final class Settings {
             POWER_MENU_PROFILES_ENABLED,
             POWER_MENU_AIRPLANE_ENABLED,
             POWER_MENU_SILENT_ENABLED,
+            POWER_MENU_USER_ENABLED,
         };
 
         /**
